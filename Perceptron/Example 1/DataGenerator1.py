6ab9abe8975a033ed7b1cba702497d71a22ced42
Dimitris Tetepoulidis <48908714+jimtete@users.noreply.github.com>
2021-04-07 12:43:42 +0300

    finished chapter I of Perceptron

--- Perceptron/Example 1/DataGenerator1.py
+++ Perceptron/Example 1/DataGenerator1.py
@@ -30,10 +30,10 @@ raw_data = {'X': numbers[:,0],
 raw_data_values = {'values':classes}
 
 df = pd.DataFrame(raw_data, columns = ['X','Y'])
-df.to_csv('./exported_data_pack_1.csv')
+df.to_csv('./exported_data_pack_1.csv',index=False)
 
 df2 = pd.DataFrame(raw_data_values, columns = ['values'] )
-df2.to_csv('./exported_data_pack_values_1.csv')
+df2.to_csv('./exported_data_pack_values_1.csv',index=False)
 
     
 """End creation of data"""
@@ -65,10 +65,10 @@ raw_data = {'X': numbers[:,0],
 raw_data_values = {'values':classes}
 
 df = pd.DataFrame(raw_data, columns = ['X','Y'])
-df.to_csv('./exported_data_pack_2.csv')
+df.to_csv('./exported_data_pack_2.csv',index=False)
 
 df2 = pd.DataFrame(raw_data_values, columns = ['values'] )
-df2.to_csv('./exported_data_pack_values_2.csv')
+df2.to_csv('./exported_data_pack_values_2.csv',index=False)
 
 """End creation of data"""
 
@@ -85,10 +85,10 @@ for i in range(n):
     if (key==0):
         numbers[i] = [(random.uniform(0.0,0.9)),(random.uniform(0.0,0.3))]
         classes[i] = 1
-    elif (key==1):
+    elif (key==2):
         numbers[i] = [(random.uniform(0.0,0.9)),(random.uniform(0.7,0.9))]
         classes[i] = 1
-    elif (key==1):
+    elif (key==3):
         numbers[i] = [(random.uniform(0.0,0.3)),(random.uniform(0.0,0.9))]
         classes[i] = 1
     elif (key==1):
@@ -104,9 +104,42 @@ raw_data = {'X': numbers[:,0],
 raw_data_values = {'values':classes}
 
 df = pd.DataFrame(raw_data, columns = ['X','Y'])
-df.to_csv('./exported_data_pack_3.csv')
+df.to_csv('./exported_data_pack_3.csv',index=False)
 
 df2 = pd.DataFrame(raw_data_values, columns = ['values'] )
-df2.to_csv('./exported_data_pack_values_3.csv')
+df2.to_csv('./exported_data_pack_values_3.csv',index=False)
+
+"""End creation of data"""
+
+n=200
+numbers = np.zeros(shape=(n,2))
+classes = np.zeros(shape=(n))
+
+
+for i in range(n):
+    key =  (random.randint(0,3))
+    if (key==0):
+        numbers[i] = [(random.uniform(0.0,0.3)),(random.uniform(0.0,0.3))]
+        classes[i] = 1
+    elif (key==1):
+        numbers[i] = [(random.uniform(0.7,0.9)),(random.uniform(0.0,0.3))]
+        classes[i] = -1
+    elif (key==2):
+        numbers[i] = [(random.uniform(0.0,0.3)),(random.uniform(0.7,0.9))]
+        classes[i] = -1
+    else:
+        numbers[i] = [(random.uniform(0.7,0.9)),(random.uniform(0.7,0.9))]
+        classes[i] = 1
+    
+raw_data = {'X': numbers[:,0],
+            'Y': numbers[:,1]}
+
+raw_data_values = {'values':classes}
+
+df = pd.DataFrame(raw_data, columns = ['X','Y'])
+df.to_csv('./exported_data_pack_4.csv',index=False)
+
+df2 = pd.DataFrame(raw_data_values, columns = ['values'] )
+df2.to_csv('./exported_data_pack_values_4.csv',index=False)
 
 """End creation of data"""
--- Perceptron/Example 1/PerceptronScript.py
+++ Perceptron/Example 1/PerceptronScript.py
@@ -4,48 +4,44 @@ Created on Mon Apr  5 15:54:12 2021
 
 @author: Dimitris
 """
-
-import random
 import numpy as np
 import matplotlib.pyplot as plt
 
 from perceptron import Perceptron
+import pandas as pd
 
-"""Creating the data 0.0:0.3 // 0.7:0.9"""
+"""Loading the data"""
 
-n=200
-numbers = np.zeros(shape=(n,2))
-classes = np.zeros(shape=(n))
+##You can only change the value below, number:1 indicates the first datapack and so on
+datapack = pd.read_csv('./exported_data_pack_4.csv')
+##End of change
 
 
-for i in range(n):
-    key =  (random.randint(0,7))
-    if (key==0):
-        numbers[i] = [(random.uniform(0.0,0.9)),(random.uniform(0.0,0.3))]
-        classes[i] = 1
-    elif (key==1):
-        numbers[i] = [(random.uniform(0.0,0.9)),(random.uniform(0.7,0.9))]
-        classes[i] = 1
-    elif (key==1):
-        numbers[i] = [(random.uniform(0.0,0.3)),(random.uniform(0.0,0.9))]
-        classes[i] = 1
-    elif (key==1):
-        numbers[i] = [(random.uniform(0.7,0.9)),(random.uniform(0.0,0.9))]
-        classes[i] = 1
-    else:
-        numbers[i] = [(random.uniform(0.4,0.6)),(random.uniform(0.4,0.6))]
-        classes[i] = -1
+numbers = datapack.to_numpy()
+numbers_train = numbers[:180]
+numbers_test = numbers[181:200]
+
+##You can only change the value below, number:1 indicates the first datapack and so on
+datapack_values = pd.read_csv('./exported_data_pack_values_4.csv')
+##End of change
+
+
+
+classes = datapack_values.to_numpy()
+classes_train = classes[:180]
+classes_test = classes[181:200]
+
     
-"""End creation of data"""
+"""End loading the data"""
 
 
 def accuracy(y_true, y_pred):
     accuracy = np.sum(y_true == y_pred) / len(y_true)
     return accuracy
 
-X_train, X_test, y_train, y_test = numbers[0:80],numbers[81:-1],classes[0:80],classes[81:-1]
+X_train, X_test, y_train, y_test = numbers_train,numbers_test,classes_train,classes_test
 
-p = Perceptron(learning_rate=0.01, n_iters=1000)
+p = Perceptron(learning_rate=0.1, n_iters=1000)
 p.fit(X_train, y_train)
 predictions = p.predict(X_test)
 
@@ -53,18 +49,19 @@ print("Perceptron classification accuracy", accuracy(y_test, predictions))
 
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
-plt.scatter(X_train[:,0], X_train[:,1],marker='o',c=y_train)
+plt.scatter(X_test[:,0], X_test[:,1],marker='o',c=y_test)
 
-x0_1 = np.amin(X_train[:,0])
-x0_2 = np.amax(X_train[:,0])
+x0_1 = np.amin(X_test[:,0])
+x0_2 = np.amax(X_test[:,0])
 
 x1_1 = (-p.weights[0] * x0_1 - p.bias) / p.weights[1]
 x1_2 = (-p.weights[0] * x0_2 - p.bias) / p.weights[1]
 
 ax.plot([x0_1, x0_2],[x1_1, x1_2], 'k')
 
-ymin = np.amin(X_train[:,1])
-ymax = np.amax(X_train[:,1])
+ymin = np.amin(X_test[:,1])
+ymax = np.amax(X_test[:,1])
 ax.set_ylim([0,1])
+ax.set_xlim([0,1])
 
 plt.show()
