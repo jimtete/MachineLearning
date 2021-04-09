6f17486ba198efb2149e82c56fc01fc98ee46fd4
Dimitris Tetepoulidis <48908714+jimtete@users.noreply.github.com>
2021-04-09 16:52:59 +0300

    added Iris Example 3

--- Perceptron/Example 1/DataGenerator1.py
+++ Perceptron/Example 1/DataGenerator1.py
@@ -171,3 +171,39 @@ df2 = pd.DataFrame(raw_data_values, columns = ['values'] )
 df2.to_csv('./exported_data_pack_values_3D1.csv',index=False)
 
 """End creation of data"""
+
+"""End creation of data"""
+
+n=200
+numbers = np.zeros(shape=(n,3))
+classes = np.zeros(shape=(n))
+
+
+for i in range(n):
+    key =  (random.randint(0,3))
+    if (key==0):
+        numbers[i] = [(random.uniform(0.0,0.3)), (random.uniform(0.0,0.3)), (random.uniform(0.0,0.3))]
+        classes[i] = 0
+    elif (key==1):
+        numbers[i] = [(random.uniform(0.7,0.9)), (random.uniform(0.7,0.9)), (random.uniform(0.7,0.9))]
+        classes[i] = 0
+    elif (key==2):
+        numbers[i] = [(random.uniform(0.7,0.9)), (random.uniform(0.7,0.9)), (random.uniform(0.0,0.3))]
+        classes[i] = 1
+    else:
+        numbers[i] = [(random.uniform(0.0,0.3)), (random.uniform(0.0,0.3)), (random.uniform(0.7,0.9))]
+        classes[i] = 1
+    
+raw_data = {'X': numbers[:,0],
+            'Y': numbers[:,1],
+            'Z': numbers[:,2]}
+
+raw_data_values = {'values':classes}
+
+df = pd.DataFrame(raw_data, columns = ['X','Y','Z'])
+df.to_csv('./exported_data_pack_3D2.csv',index=False)
+
+df2 = pd.DataFrame(raw_data_values, columns = ['values'] )
+df2.to_csv('./exported_data_pack_values_3D2.csv',index=False)
+
+"""End creation of data"""
