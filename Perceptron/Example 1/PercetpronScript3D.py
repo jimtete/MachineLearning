70810c184740f424416455fdc41bdf0c2639358c
Dimitris Tetepoulidis <48908714+jimtete@users.noreply.github.com>
2021-04-09 11:18:40 +0300

    added observation goes hmm?

--- Perceptron/Example 1/PercetpronScript3D.py
+++ Perceptron/Example 1/PercetpronScript3D.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Apr  5 15:54:12 2021
+Created on Thu Apr  8 10:42:51 2021
 
 @author: Dimitris
 """
+
 import numpy as np
 import matplotlib.pyplot as plt
-
+from mpl_toolkits import mplot3d
 from perceptron import Perceptron
 import pandas as pd
 
 """Loading the data"""
 
 ##You can only change the value below, number:1 indicates the first datapack and so on
-datapack = pd.read_csv('./exported_data_pack_4.csv')
+datapack = pd.read_csv('./exported_data_pack_1.csv')
 ##End of change
 
 
@@ -22,7 +23,7 @@ numbers_train = numbers[:180]
 numbers_test = numbers[181:200]
 
 ##You can only change the value below, number:1 indicates the first datapack and so on
-datapack_values = pd.read_csv('./exported_data_pack_values_4.csv')
+datapack_values = pd.read_csv('./exported_data_pack_values_1.csv')
 ##End of change
 
 
@@ -41,28 +42,20 @@ def accuracy(y_true, y_pred):
 
 X_train, X_test, y_train, y_test = numbers_train,numbers_test,classes_train,classes_test
 
+p = Perceptron(learning_rate=0.1, n_iters=100)
+p.fit(X_train, y_train)
+predictions = p.predict(X_test)
 
-epochs=100
-for index in range(epochs):
-    p = Perceptron(learning_rate=0.1, n_iters=index)
-    p.fit(X_train, y_train)
-    predictions = p.predict(X_test)
+print("Perceptron classification accuracy", accuracy(y_test, predictions))
 
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    plt.scatter(X_train[:,0], X_train[:,1],marker='o',c=y_train)
-    
-    x0_1 = np.amin(X_train[:,0])
-    x0_2 = np.amax(X_train[:,0])
-    
-    x1_1 = (-p.weights[0] * x0_1 - p.bias) / p.weights[1]
-    x1_2 = (-p.weights[0] * x0_2 - p.bias) / p.weights[1]
-    
-    ax.plot([x0_1, x0_2],[x1_1, x1_2], 'k')
-    
-    ymin = np.amin(X_train[:,1])
-    ymax = np.amax(X_train[:,1])
-    ax.set_ylim([0,1])
-    ax.set_xlim([0,1])
-    
-    plt.show()
+
+
+ax = plt.axes(projection='3d')
+
+z = np.linspace(0,30,100)
+x = np.sin(z)
+y = np.cos(z)
+
+ax.plot3D(x,y,z)
+
+plt.show()
--- Perceptron/TemporaryExample 1/TempPerceptronScript.py
+++ Perceptron/TemporaryExample 1/TempPerceptronScript.py
@@ -7,13 +7,13 @@ Created on Mon Apr  5 15:54:12 2021
 import numpy as np
 import matplotlib.pyplot as plt
 
-from perceptron import Perceptron
+from TempPerceptron import Perceptron
 import pandas as pd
 
 """Loading the data"""
 
 ##You can only change the value below, number:1 indicates the first datapack and so on
-datapack = pd.read_csv('./exported_data_pack_4.csv')
+datapack = pd.read_csv('./exported_data_pack_2.csv')
 ##End of change
 
 
@@ -22,7 +22,7 @@ numbers_train = numbers[:180]
 numbers_test = numbers[181:200]
 
 ##You can only change the value below, number:1 indicates the first datapack and so on
-datapack_values = pd.read_csv('./exported_data_pack_values_4.csv')
+datapack_values = pd.read_csv('./exported_data_pack_values_2.csv')
 ##End of change
 
 
@@ -42,26 +42,29 @@ def accuracy(y_true, y_pred):
 X_train, X_test, y_train, y_test = numbers_train,numbers_test,classes_train,classes_test
 
 
+#Change the epochs
 epochs=100
 for index in range(epochs):
     p = Perceptron(learning_rate=0.1, n_iters=index)
     p.fit(X_train, y_train)
     predictions = p.predict(X_test)
-
+    
+    print("Perceptron classification accuracy", accuracy(y_test, predictions))
+    
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
-    plt.scatter(X_train[:,0], X_train[:,1],marker='o',c=y_train)
+    plt.scatter(X_test[:,0], X_test[:,1],marker='o',c=y_test)
     
-    x0_1 = np.amin(X_train[:,0])
-    x0_2 = np.amax(X_train[:,0])
+    x0_1 = np.amin(X_test[:,0])
+    x0_2 = np.amax(X_test[:,0])
     
     x1_1 = (-p.weights[0] * x0_1 - p.bias) / p.weights[1]
     x1_2 = (-p.weights[0] * x0_2 - p.bias) / p.weights[1]
     
     ax.plot([x0_1, x0_2],[x1_1, x1_2], 'k')
     
-    ymin = np.amin(X_train[:,1])
-    ymax = np.amax(X_train[:,1])
+    ymin = np.amin(X_test[:,1])
+    ymax = np.amax(X_test[:,1])
     ax.set_ylim([0,1])
     ax.set_xlim([0,1])
     
