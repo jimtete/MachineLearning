a168bf35413112718a908f8acf8234340293b56b
Dimitris Tetepoulidis <48908714+jimtete@users.noreply.github.com>
2021-05-08 14:04:44 +0300

    first graph adaline

--- 2.Adaline/AdalineScript.py
+++ 2.Adaline/AdalineScript.py
@@ -2,49 +2,56 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
 
+from sklearn.model_selection import train_test_split
 from Adaline import Adaline
 import pdr
 
 
-# Παίρνω τα δεδομένα
+# Εισαγωγή δεδομένων
+
 df = pd.read_csv('./iris.data',header=None)
 
-# Πλοττάρω τα πρώτα 100 δεδομένα (setosa, versicolor)
-y = df.iloc[0:100, 4].values
-y = np.where(y == 'Iris-setosa', -1, 1)
-X = df.iloc[0:100, [0, 2]].values
 
-plt.scatter(X[:50, 0], X[:50, 1], color = 'red', marker = 'o', label = 'setosa')
-plt.scatter(X[50:100, 0], X[50:100, 1], color = 'blue', marker = 'x', label = 'versicolor')
-plt.xlabel('petal length')
-plt.ylabel('sepal length')
-plt.legend(loc = 'upper left')
-plt.show()
+data = df.iloc[:,[0,2]] ##0,2 για την πρώτη και την τρίτη στήλη
+dataValues = df.iloc[:,4]
 
-# Κανονικοποιούμε τα δεδομένα
-X_std = np.copy(X)
-X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
-X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+x_train,x_test,y_train,y_test = train_test_split(data,dataValues, test_size=0.2)
+x_train, x_test, y_train, y_test = x_train.to_numpy(), x_test.to_numpy(), y_train.to_numpy(), y_test.to_numpy()
 
-maxEpochs=150
+##Πρώτο γράφημα
 
-for index in range(maxEpochs+1):
-    # Δημιουργούμε ένα δίκτυο Adaline
-    model1 = Adaline(lr=0.001,epochs=index)
+firstClassX,firstClassY = np.array([]),np.array([])
+secondClassX,secondClassY = np.array([]),np.array([])
+thirdClassX,thirdClassY = np.array([]),np.array([])
+
+for i in range(len(x_train)):
+    x = x_train[i,0]
+    y = x_train[i,1]
     
-    # Εκπαιδεύουμε το μοντέλο
-    model1.fit(X_std, y)
-       
-    # Υπολογίζουμε την γραμμή
-    pdr.plot_decision_regions(X_std, y, classifier = model1)
-    plt.title('Adaline - Gradient Descent | epoch: '+str(index))
-    plt.xlabel('sepal length [standardized')
-    plt.ylabel('petal length [standardized]')
-    plt.legend(loc = 'upper left')
-    plt.show()
+    if (y_train[i]=='Iris-setosa'):
+        firstClassX,firstClassY = np.append(firstClassX,x),np.append(firstClassY,y)
+    elif (y_train[i]=='Iris-virginica'):
+        secondClassX,secondClassY = np.append(secondClassX,x),np.append(secondClassY,y)
+    else:
+        thirdClassX,thirdClassY = np.append(thirdClassX,x),np.append(thirdClassY,y)
 
-# Υπολογίζουμε τα Errors
-plt.plot(range(1, len(model1.cost_) + 1), model1.cost_, marker = 'o', color = 'red')
-plt.xlabel('Epochs')
-plt.ylabel('Sum-squared-error')
-plt.show()
+   
+# Γράφημα 1
+#pdr.plot_decision_regions(X_std, y, classifier = model1)
+fig = plt.figure()
+
+fig = plt.plot(firstClassX,firstClassY,"r+",label="Iris-setosa")
+fig = plt.plot(secondClassX,secondClassY,"bo",label="Iris-versicolor")
+fig = plt.plot(thirdClassX, thirdClassY, "gx",label="Iris-virginica")
+
+fig = plt.legend()
+
+# Δημιουργούμε ένα δίκτυο Adaline
+maxEpochs=100
+
+model1 = Adaline(lr=0.001,epochs=maxEpochs)
+
+# Εκπαιδεύουμε το μοντέλο
+model1.fit(x_train, y_train)
+predictions = model1.predict(x_train)
+
